82f9bd39b01aa71bb675141cfd6415ef13a4dfe9
Victorkaue333 <kaue.alves.pg@gmail.com>
2026-01-16 16:00:24 -0300

    Update Vk
    Resolução do exercício 4 em que visava resolver a Verificação de Categoria

--- 4-Verificacao_de_Categoria/index.php
+++ 4-Verificacao_de_Categoria/index.php
@@ -1,5 +1,27 @@
 <?php
+//Verificação de Categoria
+//1 - Escreva uma função chamada verificarCategoria que recebe um parâmetro de entrada representando uma categoria.
+//2 - A função deve retornar uma mensagem de acordo com a categoria fornecida:
+//  - Se a categoria for "eletrônicos", retornar "Essa categoria é de produtos eletrônicos".
+//  - Se a categoria for "vestuário", retornar "Essa categoria é de produtos de vestuário".
+//  - Se a categoria for "alimentos", retornar "Essa categoria é de produtos alimentícios".
+//  - Para qualquer outra categoria, retornar "Categoria desconhecida".
+//3 - O parâmetro de categoria será sempre fornecido como uma string.
 
+$categoria = "eletrônicos";
 
+function verificarCategoria($categoria){
+    if($categoria == "eletrônicos"){
+        echo "Essa categoria é de produtos eletrônicos";
+    } else if($categoria == "vestuário"){
+        echo "Essa categoria é de produtos de vestuário";
+    } else if($categoria == "alimentos"){
+        echo "Essa categoria é de produtos alimentícios";
+    } else {
+        echo "Categoria desconhecida";
+    }
+}
+//Chamada da função
+verificarCategoria($categoria);
 
 ?>
